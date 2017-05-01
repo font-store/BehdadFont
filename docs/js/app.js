a04ebb5176cfe292ddbc78a585dfb17b83ad3563
Saleh Souzanchi <saleh.souzanchi@gmail.com>
2017-05-01 06:46:26 +0430

    r1

---
+++ docs/js/app.js
@@ -0,0 +1,33 @@
+       $(document).ready(function () {
+
+           $("#props .owl-carousel").owlCarousel({
+               // autoPlay: true,
+               // stopOnHover: true,
+               // slideSpeed: 200,
+               // paginationSpeed: 800,
+               // rewindSpeed: 1000,
+               // navigationText: ["prev", "next"],
+               // rewindNav: true,
+               // singleItem: true,
+               // transitionStyle: "backSlide",
+               // responsive: true,
+               // responsiveRefreshRate: 200,
+               // responsiveBaseWidth: window,
+               rtl: true,
+
+               items: 1,
+               autoplay: 1000,
+               smartSpeed:200,
+               //nav: false,
+
+               animateIn: 'bounceInRight',
+               animateOut: 'bounceOutLeft',
+               //responsive: true,
+               loop: true,
+               
+               responsiveBaseElement: window
+           });
+
+
+
+       });
