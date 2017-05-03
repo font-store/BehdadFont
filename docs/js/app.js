3c9879bbc9c40e48070922b607d88d1250d8f358
Saleh Souzanchi <saleh.souzanchi@gmail.com>
2017-05-03 17:29:21 +0430

    خن

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,33 +1,43 @@
-       $(document).ready(function () {
+$(document).ready(function () {
 
-           $("#propertis .owl-carousel").owlCarousel({
-               // autoPlay: true,
-               // stopOnHover: true,
-               // slideSpeed: 200,
-               // paginationSpeed: 800,
-               // rewindSpeed: 1000,
-               // navigationText: ["prev", "next"],
-               // rewindNav: true,
-               // singleItem: true,
-               // transitionStyle: "backSlide",
-               // responsive: true,
-               // responsiveRefreshRate: 200,
-               // responsiveBaseWidth: window,
-               rtl: true,
+    $("#propertis .owl-carousel").owlCarousel({
+        // autoPlay: true,
+        // stopOnHover: true,
+        // slideSpeed: 200,
+        // paginationSpeed: 800,
+        // rewindSpeed: 1000,
+        // navigationText: ["prev", "next"],
+        // rewindNav: true,
+        // singleItem: true,
+        // transitionStyle: "backSlide",
+        // responsive: true,
+        // responsiveRefreshRate: 200,
+        // responsiveBaseWidth: window,
+        rtl: true,
 
-               items: 1,
-               autoplay: 1000,
-               smartSpeed:200,
-               //nav: false,
+        items: 1,
+        autoplay: 1000,
+        smartSpeed: 200,
+        //nav: false,
 
-               animateIn: 'bounceInRight',
-               animateOut: 'bounceOutLeft',
-               //responsive: true,
-               loop: true,
-               
-               responsiveBaseElement: window
-           });
+        animateIn: 'bounceInRight',
+        animateOut: 'bounceOutLeft',
+        //responsive: true,
+        loop: true,
 
+        responsiveBaseElement: window
+    });
 
+var worker = $('#workers');
 
-       });
+var tabItem = $('.tab-menu li',worker);
+var tabs = $('.tab-wrapper .tab-item',worker);
+tabItem.on('click',function(){
+        $(tabItem).removeClass('active');
+        var clicked = $('a',this).data('item');
+        $(tabs).removeClass('active');
+        $(clicked).addClass('active');
+        $(this).addClass('active');
+});
+
+});
